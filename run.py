d33cefae16d2411b0bac93476e4a7f028cdae41d
Zbigniew Szulc <zbigniewszulc@gmail.com>
2024-03-04 09:33:13 +0000

    Install tabulate for prettier tables display

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
 # Import modules, packages or libraries
 import gspread 
 from google.oauth2.service_account import Credentials
+from tabulate import tabulate
 
 # Declare constant variables
 SCOPE = [
@@ -14,7 +16,25 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('tape_rotation')
 
-rotation = SHEET.worksheet('Rotation')
-data = rotation.get_all_values()
-print(data)
+SYSTEM_NAME = "TAPE ROTATION MANAGEMENT SYSTEM"
+WELCOME_MSG = ("This CLI System is designed to manage and oversee the tape rotation \n"
+    "schedule across different media types, including BRMS, DAILY, WEEKLY, and MONTHLY. \n"
+    "It is tailored for IT department use, facilitating the monitoring and control of \n"
+    "backup tape storage locations, whether onsite, offsite, or retired.")
 
+def print_menu():
+    data = [
+        [1, "Engineer"],
+        [2, "Designer"],
+        [3, "Writer"]
+    ]
+    headers = ["#","Menu"]
+    table = tabulate(data, headers=headers, tablefmt="simple")
+    print(table)
+
+def main():
+    print(SYSTEM_NAME)
+    print(WELCOME_MSG)
+    print_menu()
+
+main()
