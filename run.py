f7e0bd74bb482ab5225018748d7e98a44a1a4a74
Zbigniew Szulc <zbigniewszulc@gmail.com>
2024-03-04 15:08:07 +0000

    Build menu and welcome screen

--- run.py
+++ run.py
@@ -16,25 +16,36 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('tape_rotation')
 
-SYSTEM_NAME = "TAPE ROTATION MANAGEMENT SYSTEM"
-WELCOME_MSG = ("This CLI System is designed to manage and oversee the tape rotation \n"
-    "schedule across different media types, including BRMS, DAILY, WEEKLY, and MONTHLY. \n"
+SYSTEM_NAME = 24 * " " + " TAPE ROTATION MANAGEMENT SYSTEM " + 23 * " "
+WELCOME_MSG = ("This CLI System is designed to manage and oversee the tape rotation schedule\n"
+    "across different media types, including BRMS, DAILY, WEEKLY, and MONTHLY. \n"
     "It is tailored for IT department use, facilitating the monitoring and control of \n"
     "backup tape storage locations, whether onsite, offsite, or retired.")
 
+def print_welcome_screen():
+    print(80 * "=")
+    print(SYSTEM_NAME)
+    print(80 * "=")
+    print(WELCOME_MSG)
+    print(80 * "-" + "\n")
+
 def print_menu():
     data = [
-        [1, "Engineer"],
-        [2, "Designer"],
-        [3, "Writer"]
+        [1, "Move tape offsite"],
+        [2, "Move tape onsite"],
+        [3, "Move tape to retired media pool"],
+        [4, "Display all tapes stored offsite"],
+        [5, "Display all tapes stored onsite"],
+        [6, "Display all retired tapes"],
+        [7, "Lookup"],
+        [8, "Exit"]
     ]
     headers = ["#","Menu"]
     table = tabulate(data, headers=headers, tablefmt="simple")
     print(table)
 
 def main():
-    print(SYSTEM_NAME)
-    print(WELCOME_MSG)
+    print_welcome_screen()
     print_menu()
 
 main()
